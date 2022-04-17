14c869cb4ccff6796e07299bebf7ad65a42cc106
Guerra24 <lili@guerra24.net>
2022-04-16 20:30:57 -0600

    Actions!

---
+++ Util/PrepareForUpload.ps1
@@ -0,0 +1,8 @@
+﻿[xml]$xmlDoc = Get-Content "./LANraragi/Package.appxmanifest"
+
+Set-Location "./LANraragi/AppPackages/"
+Rename-Item $(Get-ChildItem *LANraragi* -Directory) "$(Get-Location)/LANraragi"
+
+Set-Location "./LANraragi"
+Remove-Item -Path "./Install.ps1","./Add-AppDevPackage.ps1"
+Remove-Item -Path "./Add-AppDevPackage.resources" –recurse
---
+++ Util/Version.ps1
@@ -0,0 +1,14 @@
+﻿$xmlFileName = "LANraragi\Package.appxmanifest"      
+[xml]$xmlDoc = Get-Content $xmlFileName
+
+$count = $args[0]
+$version = [System.Version]::Parse($xmlDoc.Package.Identity.Version);
+
+$Field = $version.GetType().GetField('_Revision', 'static,nonpublic,instance')
+$Field.SetValue($version, $count -as [int])
+
+$xmlDoc.Package.Identity.Version = $version.ToString();
+
+echo "Version:" $xmlDoc.Package.Identity.Version
+
+$xmlDoc.Save("$(Get-Location)/$xmlFileName")
